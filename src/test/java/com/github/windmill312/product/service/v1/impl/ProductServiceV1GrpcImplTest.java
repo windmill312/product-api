e7f9a6f8044346d91cd66d086ae48cf200c7c42a
a.sychev <a.sychev@qiwi.com>
2020-02-27 17:10:47 +0300

    [TF] Add negative test for TeamCity

--- src/test/java/com/github/windmill312/product/service/v1/impl/ProductServiceV1GrpcImplTest.java
+++ src/test/java/com/github/windmill312/product/service/v1/impl/ProductServiceV1GrpcImplTest.java
@@ -127,7 +127,7 @@ public class ProductServiceV1GrpcImplTest {
 
     @Test
     public void testForTeamCity() {
-        Assert.assertTrue(false);
+        Assert.assertTrue(true);
     }
 
     private ProductEntity getMockObjСafeEntity() {
